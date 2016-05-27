f650726ccb817d5f395e7a600bfb4e7d978d6a8c
martinaybar <martinaybar@gmail.com>
2016-05-27 16:38:51 -0300

    Add project files.

---
+++ OwinAuthenticationExample/Models/UserIdentification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OwinAuthenticationExample.Models
+{
+    public class UserIdentification
+    {
+        public int Id { get; set; }
+        public string FirtName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+
+    }
+}
