e996d227169aa12a9dd1f51ef7b6ee9e752ee4a6
martinaybar <martinaybar@gmail.com>
2016-05-27 16:41:35 -0300

    iii

--- OwinAuthenticationExample/Models/ManageViewModels.cs
+++ OwinAuthenticationExample/Models/ManageViewModels.cs
@@ -33,6 +33,7 @@ namespace OwinAuthenticationExample.Models
         [Display(Name = "New password")]
         public string NewPassword { get; set; }
 
+
         [DataType(DataType.Password)]
         [Display(Name = "Confirm new password")]
         [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
